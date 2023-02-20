0df6038d4c54a04afe81aca3f68b0d60061ab058
Nizzo07 <afonso7557@gmail.com>
2023-02-20 11:31:45 +0000

    20/02

---
+++ Ficha1.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Ex 3
+//1)
+
+/*void IIIi () {
+    int x, y;
+    int i, j;
+    printf("Draw a rectangle:\n\n");
+   printf("Whidth: ");
+    scanf("%d",&x);
+    printf("Height: ");
+    scanf("%d",&y);
+
+    for(i = 0; i < x; i++)
+        printf("#"); {
+
+
+       for(j = 0; j < y; j++)
+            printf("#");
+
+        }
+        print("\n");
+
+
+    return 0;
+}
+*/
+
+
+void IIIii () {
+
+
+
+}
+
+
+
+void IIIiii () {
+
+    
+
+
+
+    return 0;
+
+}
+
+
+
+
+void IIIi () {
+
+int x,y,w;
+
+printf("Numero \n");
+scanf ("%d", &x);
+y = 0;
+while(y < x)
+{
+     w = 0;
+while(w < x)
+{
+    printf("#");
+    w = w +1;
+
+}
+printf("\n");
+y = y + 1;
+}
+
+
+
+
+
+
+}
+
+
+
+
+
+int main () {
+
+    IIIi();
+   
+}
