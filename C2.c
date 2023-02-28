1c4b8740909e1aeaf147215b29bbc759db48bca5
Nizzo07 <afonso7557@gmail.com>
2023-02-28 10:20:53 +0000

    Arroz

---
+++ C2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int abx, ord;
+    int x = 0, y = 0;
+
+    
+    if(scanf("%d", &abx) != 1){
+        return 1;
+    }
+
+    if(scanf("%d", &ord) != 1){
+        return 1;
+    }
+
+    
+
+    return 0;
+}
