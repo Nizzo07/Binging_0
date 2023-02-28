21182f8d232d63a4cced5a0e76c330d0318c6057
Nizzo07 <afonso7557@gmail.com>
2023-02-28 10:44:03 +0000

    Arroz

--- la.exe/C2.c
+++ la.exe/C2.c
@@ -6,17 +6,31 @@ int main()
 
     int abx, ord;
     int x = 0, y = 0;
+    for (int i; i < 5; i++)
+    {
 
-    
-    if(scanf("%d", &abx) != 1){
-        return 1;
+        if (scanf("%d", &abx) != 1)
+        {
+            return 1;
+        }
+
+        if (scanf("%d", &ord) != 1)
+        {
+            return 1;
+        }
     }
+    if (ord % 2 = 0)
+        y = ord + y;
 
-    if(scanf("%d", &ord) != 1){
-        return 1;
-    }
+    else if (ord % ord = 1)
+        y = ord + y;
 
-    
+    if (abx % 4 = 0)
+        x = abx + x;
+    else if (abx % abx = 1)
+        x = abx + x;
+
+    printf("%d %d", abx, ord);
 
     return 0;
 }
