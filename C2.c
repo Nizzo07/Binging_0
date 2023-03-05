12a2e3330da333d27ac32f7d0df2e2e12f1fba56
Nizzo07 <afonso7557@gmail.com>
2023-03-05 11:29:28 +0000

    Bruh

--- C2.c
+++ C2.c
@@ -3,6 +3,35 @@
 
 int main()
 {
+    // wi - wins Inzel, wc - wins CMD, d - draws
+    int n;
+    char z, c;
+    int wi = 0, wc = 0, d = 0;
+
+    if (scanf("%d", &n) != 1)
+    {
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (scanf("%c %c", &z, &c) != 2)
+        {
+            return 2;
+        }
+        if(z == '@' && c == '+')
+        {
+            wi++;
+        } else {
+            if( z == '@' && c == '-'){
+                wc++;
+            } 
+            
+        }
+       
+    }
+
+    printf("%d %d %d", wi, wc, d);
 
     return 0;
 }
