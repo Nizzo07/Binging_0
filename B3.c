3d054569b5e514aa2928734ee719622f993c7747
Nizzo07 <afonso7557@gmail.com>
2023-03-06 14:37:01 +0000

    Bruh

---
+++ B3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int x = 5;
+    int C, B, D, E;
+    int n;
+
+    if(scanf("%d", &n) != 1){
+        return 1;
+    }
+    for(int i = 0; i < n; i++){
+    
+
+    }
+    return 0;
+}
