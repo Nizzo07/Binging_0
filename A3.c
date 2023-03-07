2fec5633dce83762c12e1c3c759d017ba1579b14
Nizzo07 <afonso7557@gmail.com>
2023-03-07 10:02:43 +0000

    Bruh

--- A3.c
+++ A3.c
@@ -2,9 +2,21 @@
 #include <stdlib.h>
 
 int main () 
-{
+{    
+    int n;    
+    char chave[8];
+    char mensage[n];
 
+    if(scanf("%d", &n) != 1){
+        return 1;
+    }
 
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%s", chave);
+    }
+
+    
 
     return 0;
 }
--- B3.c
+++ B3.c
@@ -6,6 +6,7 @@ int main()
 {
   int n;
   int posicao = 5;
+  char mov;
 
   if(scanf("%d",&n) != 1)
   {
@@ -13,7 +14,11 @@ int main()
   }
     for(int i = 0; i < n; i++)
     {
-        
+        if(scanf("%c",&mov) != 1)
+        {
+            return 1;
+   
+
     }
 
 
