47c7f087eb49831141ba22f4389c8f8e9aaa534c
Nizzo07 <afonso7557@gmail.com>
2023-03-08 19:14:15 +0000

    Bruh

---
+++ miniT.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+  int i, j;
+ 
+
+   int contador = 0;
+  for(i = 8; i < 13; i ++){
+    for(j = 8; j < 13; j ++){ 
+        contador++; 
+    }
+
+  }
+    printf("%d",contador);
+  return 0;
+}
+
+
+    
---
+++ miniT2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+  int i;
+    int contador = 0;
+  for(i = 4; i < 28; i+= 3){
+     contador++;
+  }
+     printf("%d",contador);
+  return 0;
+}
