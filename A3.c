4e3dfa7ac712900617c467c85c9ed223c722c1aa
Nizzo07 <afonso7557@gmail.com>
2023-03-08 18:42:42 +0000

    Bruh

--- A3.c
+++ A3.c
@@ -1,22 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main () 
-{    
-    int n;    
-    char chave[8];
-    char mensage[n];
 
-    if(scanf("%d", &n) != 1){
-        return 1;
-    }
 
-    for(int i = 0; i < n; i++)
-    {
-        scanf("%s", chave);
-    }
 
-    
 
-    return 0;
-}
+
