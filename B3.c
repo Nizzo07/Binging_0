6e026877b208f47f36e2d1c6bc13d2790aa4c43d
Nizzo07 <afonso7557@gmail.com>
2023-03-08 17:18:33 +0000

    Bruh

--- B3.c
+++ B3.c
@@ -3,24 +3,28 @@
 #include <string.h>
 
 int main()
-{
-  int n;
-  int posicao = 5;
-  char mov;
 
-  if(scanf("%d",&n) != 1)
-  {
-    return 1;
-  }
-    for(int i = 0; i < n; i++)
-    {
-        if(scanf("%c",&mov) != 1)
-        {
-            return 1;
-   
+ {
+    int n, c = 5;
+    char keypad [3][3] = {{1,2,3},{4,5,6},{7,8,9}};
 
-    }
+   if(scanf("%d", &n) != 1){
+      return 1;
+   }
+
+   for(int i = 0; i < n; i++)
+   {
+      for(int j = 0; j < i; j++)
+      {
+         printf("%c", keypad[i][j]);
+      }
+   }
+
+   printf("    ")
 
 
     return 0;
-}
+ }
+
+
+
--- PI.c
+++ PI.c
@@ -4,61 +4,63 @@
 
 // 1)
 
-void I() {
+void I()
+{
 
     int x;
     int y = 0;
 
-    
-    while(1){   
-        
+    while (1)
+    {
+
         printf("Enter number: ");
-        
+
         scanf("%d", &x);
-        
-        if (x == 0) break;
 
-        if (x > y) y = x;               
+        if (x == 0)
+            break;
+
+        if (x > y)
+            y = x;
     }
-
 
     printf("%d \n", y);
 
     return 0;
 }
 
-// Defina um programa que lˆe (usando a fun¸c˜ao scanf uma sequˆencia de n´umeros inteiros terminada com o n´umero 0 e imprime no ecran a m´edia da sequˆenc
-void II () {
-
-    int x;
-    int y;
-    int media;
-
-
-        do {
-            printf("Enter a number: ");
-            scanf("%d",&x);
-            media = ((x + y) / 2);
-
-                if (x == 0) break;
-
-        } while(x != 0);
-
-        printf("The value is %d", x);
-    }
-
-
-
-
-
-
-
-
-
-
-
-int main()
+// Defina um programa que lˆe (usando a fun¸c˜ao scanf uma sequˆencia de n´umeros inteiros terminada com o n´umero 0 e imprime no ecran a m´edia da sequˆenc
+void II()
 {
 
-    II();
-}
+  int x, n, media;
+
+  if(scanf("%d", &n) != 1){
+    return 1;
+  }
+
+  for(int i = 0; i < n; i++)
+  {
+    if(scanf("%d", &x) != 1)
+    {
+        return 1;
+    }
+    if(x == 0)
+    break; 
+  
+  }
+
+  
+
+    
+    
+    
+    
+    
+}   
+    
+    int main()
+    {
+
+        II();
+    }
