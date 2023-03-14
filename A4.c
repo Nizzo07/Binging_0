29269c17897a24c723d758ac6dbab7a46d5fd0f5
Nizzo07 <afonso7557@gmail.com>
2023-03-14 10:03:22 +0000

    Bruh

--- A4.c
+++ A4.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main () 
-{
-    
+int main() {
+    int n;
+    char line[1001];
 
+    // Lê o número de casos
+    fgets(line, 1001, stdin);
+    sscanf(line, "%d", &n);
 
+    // Lê as N linhas contendo as palavras
+    for (int i = 0; i < n; i++) {
+        fgets(line, 1001, stdin);
 
-
+        // Separa as palavras
+        char *token = strtok(line, " ");
+        while (token != NULL) {
+            printf("%s\n", token);
+            token = strtok(NULL, " ");
+        }
+    }
 
     return 0;
 }
