9b5f2fda4f62e3a005a498a871f8626381a520bc
Nizzo07 <afonso7557@gmail.com>
2023-03-14 17:40:12 +0000

    Bruh

---
+++ fds2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void arrays () 
+{
+    int pares[5] = {0,2,4,6,8};
+
+    printf("%d\n", pares[0]);
+    printf("%d\n", pares[1]);
+    printf("%d\n", pares[2]);
+    printf("%d\n", pares[3]);
+    printf("%d\n", pares[4]);
+}
+
+
+
+
+
+
+
+
+
+
+
+int main ()
+{
+
+    arrays ();
+
+
+    return 0;
+}
